d30e23f24a9b7557565d0fc6c74f2124009c8545
Vladyslav Filat <vladislavfilat095@gmail.com>
2025-11-04 17:06:26 +0100

    make build

---
+++ js_task_parse_list_DOM.f75de5e1.js
@@ -0,0 +1,37 @@
+'use strict';
+function parseSalary(salaryStr) {
+    return Number(salaryStr.split('$').join('').split(',').join(''));
+}
+function sortList(parentElement) {
+    const items = [
+        ...parentElement.querySelectorAll('li')
+    ];
+    items.sort((a, b)=>{
+        const salaryA = parseSalary(a.getAttribute('data-salary'));
+        const salaryB = parseSalary(b.getAttribute('data-salary'));
+        return salaryB - salaryA;
+    });
+    items.forEach((item)=>parentElement.appendChild(item));
+}
+function getEmployees(parentElement) {
+    const items = [
+        ...parentElement.querySelectorAll('li')
+    ];
+    return items.map((item)=>{
+        const salary = parseSalary(item.getAttribute('data-salary'));
+        return {
+            name: item.textContent.trim(),
+            position: item.getAttribute('data-position'),
+            salary,
+            age: Number(item.getAttribute('data-age'))
+        };
+    });
+}
+// 🔹 Виклики
+const list = document.querySelector('ul');
+sortList(list);
+const employees = getEmployees(list);
+// eslint-disable-next-line no-console
+console.log(employees);
+
+//# sourceMappingURL=js_task_parse_list_DOM.f75de5e1.js.map
